a757a73f8ac41ff28e94238a2baf8dd5fe08441f
l1n4n <35434784+l1n4n@users.noreply.github.com>
2018-03-15 06:54:27 -0400

    Delete fib_fast.py

--- W2/fib_fast.py
+++
@@ -1,9 +0,0 @@
-# Uses python3
-def calc_fib(n):
-    fib = [0, 1] + [0] * (n - 1)
-    for i in range(2, n + 1):
-    	fib[i] = fib[i - 1] + fib[i - 2]
-    return fib[n]
-
-n = int(input())
-print(calc_fib(n))
